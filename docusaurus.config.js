7b63a74f71392d5aa70b7f51973f7ea635513fc4
hahajoke865 <1184890415@qq.com>
2022-11-07 12:26:29 +0800

    second

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://github.com/hahajoke865/Wiki_kang.git',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
